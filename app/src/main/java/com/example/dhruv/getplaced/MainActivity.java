badbb76f55b39cf66e969681dc515559ca238373
Dhruv Jaglan <dhruvjaglan2@gmail.com>
2017-09-27 14:10:44 +0530

    Linked SSO with student home

--- app/src/main/java/com/example/dhruv/getplaced/MainActivity.java
+++ app/src/main/java/com/example/dhruv/getplaced/MainActivity.java
@@ -1,20 +1,23 @@
 package com.example.dhruv.getplaced;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
-import java.net.URL;
 
 import javax.net.ssl.HttpsURLConnection;
 
@@ -23,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
     private Button student_login;
     private String URL;
     private String AUTHORIZATION_CODE;
+    private EditText edit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,14 +50,18 @@ public class MainActivity extends AppCompatActivity {
                 AlertDialog.Builder SSO_login = new AlertDialog.Builder(MainActivity.this);
                 View SSO_view = getLayoutInflater().inflate(R.layout.sso_page, null);
                 WebView SSO_page = (WebView) SSO_view.findViewById(R.id.sso_login);
+                edit = (EditText) SSO_view.findViewById(R.id.editText);
+                edit.setFocusable(true);
+                edit.requestFocus();
                 SSO_page.loadUrl(URL);
                 SSO_login.setView(SSO_view);
-                final AlertDialog dialog = SSO_login.create();
+                final Dialog dialog = SSO_login.create();
                 dialog.show();
                 SSO_page.setWebViewClient(new WebViewClient() {
                     @Override
                     public boolean shouldOverrideUrlLoading(WebView view, String url) {
                         view.loadUrl(url);
+
                         if(url.contains("https://www.google.co.in/")){
                             if(url.contains("error=access_denied")){
                                 Toast.makeText(getApplicationContext(),"Authorization Unsuccessful",Toast.LENGTH_SHORT).show();
@@ -61,6 +69,9 @@ public class MainActivity extends AppCompatActivity {
                             else{
                                 Toast.makeText(getApplicationContext(),"Authorization Sucessful",Toast.LENGTH_SHORT).show();
                                 AUTHORIZATION_CODE = getQueryMap(url);
+                                Intent i = new Intent(MainActivity.this, student_home.class);
+                                startActivity(i);
+                                //sendPost(AUTHORIZATION_CODE);
                             }
                             dialog.dismiss();
                         }
@@ -89,7 +100,7 @@ public class MainActivity extends AppCompatActivity {
         return map.get("code");
     }
 
-    private void sendPost(String Key) throws Exception {
+    private String sendPost(String Key) throws Exception {
 
         String url = "https://gymkhana.iitb.ac.in/sso/oauth/token/";
         URL obj = new URL(url);
@@ -124,7 +135,7 @@ public class MainActivity extends AppCompatActivity {
         in.close();
 
         //print result
-        System.out.println(response.toString());
+        return response.toString();
 
     }
 }
--- app/src/main/java/com/example/dhruv/getplaced/companylogin.java
+++ app/src/main/java/com/example/dhruv/getplaced/companylogin.java
@@ -1,18 +1,14 @@
 package com.example.dhruv.getplaced;
 
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.content.Intent;
-
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
